46ff202757a01d6b49ef2d4c6161c414665d54ba
arodri202 <angelrodriguez99202@gmail.com>
2020-05-02 22:51:29 -0400

    init

--- research/attention_ocr/python/datasets/__init__.py
+++ research/attention_ocr/python/datasets/__init__.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-# 
-# import fsns
-# import fsns_test
-# import number_plates
 #
-# __all__ = [fsns, fsns_test, number_plates]
+import fsns
+import fsns_test
+import number_plates
+
+__all__ = [fsns, fsns_test, number_plates]
